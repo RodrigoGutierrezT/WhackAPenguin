b6692a7227b7c289ac178a381bb44b7ec3781e19
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-11-08 19:33:51 -0300

    feat: Initial commit

---
+++ WhackAPenguin/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  WhackAPenguin
+//
+//  Created by Rodrigo on 08-11-24.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+}
