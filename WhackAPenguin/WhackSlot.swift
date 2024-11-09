a7ab1117ac3422a778f79b84decd26f78ef6d3f2
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-11-09 19:24:59 -0300

    feat: Add CropNode and penguins to the GameScene

--- WhackAPenguin/WhackSlot.swift
+++ WhackAPenguin/WhackSlot.swift
@@ -9,11 +9,27 @@ import SpriteKit
 import UIKit
 
 class WhackSlot: SKNode {
+    var charNode: SKSpriteNode!
     
     func configure(at position: CGPoint) {
         self.position = position
         
         let sprite = SKSpriteNode(imageNamed: "whackHole")
         addChild(sprite)
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        // show the image of the child node that is inside the mask range
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
+        
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        // chardNode(penguin) gets added to cropNode as child
+        cropNode.addChild(charNode)
+        
+        // then cropNode as a whole gets added to WhackSlot
+        addChild(cropNode)
     }
 }
